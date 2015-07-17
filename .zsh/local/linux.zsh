88af1e878d5f112e82a80b55ff684c67d3165cfc
nsi <nsi@example.com>
2015-07-17 16:16:06 -0400

    mintty:	nicer, easier to read background
    zsh:
    	cleaned up aliases
    	better modularisation

---
+++ .zsh/local/linux.zsh
@@ -0,0 +1,8 @@
+if [ [${HOST} == "teragrid"] && [$TG_HOME] ]; then
+# -----------------------------------------------
+# Lab-specific variables
+# -----------------------------------------------
+
+	source ~/.zsh/local/teragrid.zsh
+fi
+
