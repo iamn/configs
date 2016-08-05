83fb7c3f946d1acbc1d267d104399b827e647fd0
Nikita Imennov <nikita_imennov@ssga.com>
2016-08-05 14:01:10 -0400

    remove references to teragrid, obviating all content in linux.zsh clean up .zshrc w/o any meaningful changes

--- .zsh/local/linux.zsh
+++
@@ -1,8 +0,0 @@
-if [ [${HOST} == "teragrid"] && [$TG_HOME] ]; then
-# -----------------------------------------------
-# Lab-specific variables
-# -----------------------------------------------
-
-	source ~/.zsh/local/teragrid.zsh
-fi
-
